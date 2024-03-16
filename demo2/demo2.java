b83257289d53752b8b59b3cad77b9d562325f34d
Chirantan8 <chirantanchaudhari27@gmail.com>
2024-03-16 19:42:56 +0530

    Initial commit

---
+++ demo2/demo2.java
@@ -0,0 +1,8 @@
+class Demo2 {
+	public static void main(String[] args){
+		System.out.println("Hello");
+	}
+}
+
+
+// lecture.3 3-01-2024
---
+++ demo3/demo3.java
@@ -0,0 +1,7 @@
+class Demo3 {
+	public static void main(String[] args){
+		System.out.println("Welcome to Core2web");
+	}
+}
+
+// lecture.5 9-01-2024
